dd443150ca0eb3ea26c390078df02336f0139fc8
Yan-Rong Li <liyanrong@mail.ihep.ac.cn>
2023-11-18 10:58:59 +0800

    imporove rebin_spectrum.

--- src/pyat/rebin_spectrum.py
+++ src/pyat/rebin_spectrum.py
@@ -27,15 +27,17 @@ def rebin_spectrum(wave, prof, wave_rebin):
   wave_rebin_edge[-1]  = wave_rebin[-1]  + 0.5*(wave_rebin[-1]-wave_rebin[-2])
 
   prof_rebin = np.zeros(len(wave_rebin))
+  idx_left=0
+  idx_right=0
   for i in range(len(wave_rebin)):
     wbin_left, wbin_right = wave_rebin_edge[i:i+2]
-    idx_left = np.searchsorted(wave_edge, wbin_left)
-    idx_right = np.searchsorted(wave_edge, wbin_right)
+    idx_left = np.searchsorted(wave_edge[idx_left:], wbin_left) + idx_left
+    idx_right = np.searchsorted(wave_edge[idx_right:], wbin_right) + idx_right
     
-    # print(i, idx_left, idx_right)  
+    print(i, idx_left, idx_right)  
      
     if idx_left == idx_right:  # in the same bin of wave
-      idx = max(0, idx_left-1)
+      idx = min(max(0, idx_left-1), len(wave)-1) # make sure idx in the approporite range 
       prof_rebin[i] = prof[idx]
 
     else: # not in the same bin of wave
@@ -45,29 +47,29 @@ def rebin_spectrum(wave, prof, wave_rebin):
       for j in range(idx_left, idx_right-1):
         flux += prof[j] * (wave_edge[j+1] - wave_edge[j])
       # rightmost bin
-      idx = min(len(wave)-1, idx_right-1)
+      idx = max(0, min(len(wave)-1, idx_right-1)) # make sure idx in the approporite range 
       flux += (wave_rebin_edge[i+1] - wave_edge[idx_right-1]) * prof[idx]
 
       prof_rebin[i] = flux / (wave_rebin_edge[i+1]-wave_rebin_edge[i])
 
-  # x = np.array(list(zip(wave_edge[:-1], wave_edge[1:]))).flatten()
-  # y = np.array(list(zip(prof, prof))).flatten()
-  # plt.plot(x, y)
-  # x = np.array(list(zip(wave_rebin_edge[:-1], wave_rebin_edge[1:]))).flatten()
-  # y = np.array(list(zip(prof_rebin, prof_rebin))).flatten()
-  # plt.plot(x, y)
-  # plt.show()
+  x = np.array(list(zip(wave_edge[:-1], wave_edge[1:]))).flatten()
+  y = np.array(list(zip(prof, prof))).flatten()
+  plt.plot(x, y)
+  x = np.array(list(zip(wave_rebin_edge[:-1], wave_rebin_edge[1:]))).flatten()
+  y = np.array(list(zip(prof_rebin, prof_rebin))).flatten()
+  plt.plot(x, y)
+  plt.show()
 
-  # # check flux
-  # flux = 0.0
-  # for i in range(len(wave)):
-  #   flux += prof[i] * (wave_edge[i+1]-wave_edge[i])
-  # print(flux)
+  # check flux
+  flux = 0.0
+  for i in range(len(wave)):
+    flux += prof[i] * (wave_edge[i+1]-wave_edge[i])
+  print(flux)
 
-  # flux = 0.0
-  # for i in range(len(wave_rebin)):
-  #   flux += prof_rebin[i] * (wave_rebin_edge[i+1]-wave_rebin_edge[i])
-  # print(flux)
+  flux = 0.0
+  for i in range(len(wave_rebin)):
+    flux += prof_rebin[i] * (wave_rebin_edge[i+1]-wave_rebin_edge[i])
+  print(flux)
 
   return prof_rebin
 
