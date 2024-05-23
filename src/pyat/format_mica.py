f21cafbd5985065be97275f7e6ed60f16365df4a
Yan-Rong Li <liyanrong@mail.ihep.ac.cn>
2024-05-23 16:47:16 +0800

    imporove format_mica for mulitple datasets.

--- src/pyat/format_mica.py
+++ src/pyat/format_mica.py
@@ -23,24 +23,56 @@ def format_mica(fname, data1, data2):
     No returns.
   """
 
-  if not isinstance(data2, list):
-    fp = open(fname, "w")
-    fp.write("# 1\n")
-    fp.write("# %d:%d\n"%(data1.shape[0], data2.shape[0]))
-    np.savetxt(fp, data1, fmt="%f")
-    fp.write("\n")
-    np.savetxt(fp, data2, fmt="%f")
-    fp.close()
+  # data1 has only one set
+  if not isinstance(data1, list):
+    if not isinstance(data2, list):
+      fp = open(fname, "w")
+      fp.write("# 1\n")
+      fp.write("# %d:%d\n"%(data1.shape[0], data2.shape[0]))
+      np.savetxt(fp, data1, fmt="%f")
+      fp.write("\n")
+      np.savetxt(fp, data2, fmt="%f")
+      fp.close()
+    else:
+      fp = open(fname, "w")
+      fp.write("# 1\n")
+      fp.write("# %d"%data1.shape[0])
+      for i in range(len(data2)):
+        fp.write(":%d"%data2[i].shape[0])
+      fp.write("\n")
+      np.savetxt(fp, data1, fmt="%f")
+      for i in range(len(data2)):
+        fp.write("\n")
+        np.savetxt(fp, data2[i], fmt="%f")
+      
+      fp.close()
   else:
     fp = open(fname, "w")
-    fp.write("# 1\n")
-    fp.write("# %d"%data1.shape[0])
-    for i in range(len(data2)):
-      fp.write(":%d"%data2[i].shape[0])
-    fp.write("\n")
-    np.savetxt(fp, data1, fmt="%f")
-    for i in range(len(data2)):
-      fp.write("\n")
-      np.savetxt(fp, data2[i], fmt="%f")
+
+    # write header
+    fp.write("# %d\n"%len(data1))
+    for i in range(len(data1)):
+      fp.write("# %d"%data1[i].shape[0])
+      if not isinstance(data2[i], list):
+        fp.write(":%d\n"%data2[i].shape[0])
+      else:
+        for j in range(len(data2[i])):
+          fp.write(":%d"%data2[i][j].shape[0])
+        fp.write("\n")
+    # write data
+    ic = 0
+    for i in range(len(data1)):
+      if ic != 0:
+        fp.write("\n")
+      np.savetxt(fp, data1[i], fmt="%f")
+      if not isinstance(data2[i], list):
+        fp.write("\n")
+        np.savetxt(fp, data2[i], fmt="%f")
+      else:
+        for j in range(len(data2[i])):
+          fp.write("\n")
+          np.savetxt(fp, data2[i][j], fmt="%f")
+      
+      ic += 1
     
     fp.close()
