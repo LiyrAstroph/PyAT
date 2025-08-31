0182872a063d5e0d96ff6531db2a934c1b9109ca
Yan-Rong Li <lyanrong@mail.ihep.ac.cn>
2025-08-31 16:39:35 +0800

    update spec_merge.py

--- src/pyat/spec_merge.py
+++ src/pyat/spec_merge.py
@@ -5,7 +5,7 @@ from pyat import rebin_spectrum, rebin_spectrum_with_error
 
 TINY = 1.0e-50
 
-def spec_merge(wave_list, flux_list, err_list=None):
+def spec_merge(wave_list, flux_list, err_list=None, scale=False, SN_limit=None):
     """
     Merge multiple spectra into a single spectrum.
 
@@ -17,6 +17,10 @@ def spec_merge(wave_list, flux_list, err_list=None):
         Flux arrays for each spectrum.
     err_list : list of 1D array-like, optional
         Error arrays for each spectrum. If provided, will be used to compute the merged error.
+    scale : bool, optional
+        If True, the merged spectrum will be scaled to match the flux of the individual spectra.
+    SN_limit: float, optional
+        Signal-to-noise ratio limit for including individual spectral pixel in the merged spectrum.
 
     Returns
     -------
@@ -34,22 +38,65 @@ def spec_merge(wave_list, flux_list, err_list=None):
     
     if err_list is not None and len(err_list) != len(wave_list):
         raise ValueError("If provided, err_list must have the same length as wave_list and flux_list.")
-
-    wave = wave_list[0]
-    flux = flux_list[0]
-    if err_list is not None:
-        err = err_list[0] 
-    else:
-        err = np.zeros(len(flux))  # Initialize err if not provided
-
-    for i in range(1, len(wave_list)):
-        wave_i = wave_list[i]
-        flux_i = flux_list[i]
-
-        if err_list is not None:
-            err_i = err_list[i]
+    
+    if SN_limit is not None and err_list is None:
+        raise ValueError("err_list must be provided if SN_limit is set.")
+    
+    # first sort the wave_list
+    wave_sort = np.zeros(len(wave_list))
+    for i in range(len(wave_list)):
+        if len(wave_list[i]) != 0:
+            wave_sort[i] = wave_list[i][0]
         else:
-            err_i = np.zeros(len(flux_i))  # Initialize err_i if not provided
+            wave_sort[i] = np.inf
+    arg_sort = np.argsort(wave_sort)
+    wave_list = [wave_list[i] for i in arg_sort]
+    flux_list = [flux_list[i] for i in arg_sort]
+    err_list = [err_list[i] for i in arg_sort] if err_list is not None else None
+
+    i_start = 1
+    for i in range(len(wave_list)):
+        if len(wave_list[i]) != 0:
+            if SN_limit is None:
+                wave = wave_list[i]
+                flux = flux_list[i]
+                if err_list is not None:
+                    err = err_list[i]
+                else:
+                    err = np.zeros(len(flux))  # Initialize err if not provided
+            else:
+                sn = flux_list[i] / (err_list[i] + TINY)
+                mask = (sn >= SN_limit) & (err_list[i] > 0)
+                wave = wave_list[i][mask]
+                flux = flux_list[i][mask]
+                err = err_list[i][mask]
+
+            i_start = i+1
+            break
+        i_start += 1
+    
+    if i_start == len(wave_list) + 1:
+        raise ValueError("All input spectra are empty.")
+
+    for i in range(i_start, len(wave_list)):
+
+        if len(wave_list[i]) == 0:
+            continue
+        
+        if SN_limit is None:
+            wave_i = wave_list[i]
+            flux_i = flux_list[i]
+
+            if err_list is not None:
+                err_i = err_list[i]
+            else:
+                err_i = np.zeros(len(flux_i))  # Initialize err_i if not provided
+        else:
+            sn = flux_list[i] / (err_list[i] + TINY)
+            mask = (sn >= SN_limit) & (err_list[i] > 0)
+            wave_i = wave_list[i][mask]
+            flux_i = flux_list[i][mask]
+            err_i = err_list[i][mask]
 
         overlap_mask_0 = (wave >= wave_i[0]) & (wave <= wave_i[-1])
         overlap_mask_i = (wave_i >= wave[0]) & (wave_i <= wave[-1])
@@ -60,6 +107,12 @@ def spec_merge(wave_list, flux_list, err_list=None):
             flux = np.append(flux, flux_i)
             if err_list is not None:
                 err = np.append(err, err_i)
+            
+            arg_sort = np.argsort(wave)
+            wave = wave[arg_sort]
+            flux = flux[arg_sort]
+            err = err[arg_sort]
+
             continue
 
         # check which spectrum is is finer 
@@ -92,20 +145,32 @@ def spec_merge(wave_list, flux_list, err_list=None):
         
         if err_list is not None:
             flux_rebin, err_rebin = rebin_spectrum_with_error(wave_0[overlap_mask], wave_m, flux_m, err_m)
-            flux_0[overlap_mask] = np.average((flux_0[overlap_mask], flux_rebin), axis=0, 
-                                               weights=(1/(err_0[overlap_mask]**2+TINY),  1/(err_rebin**2+TINY)))
-            err_0[overlap_mask] = np.sqrt(np.average((err_0[overlap_mask]**2, err_rebin**2), axis=0,
-                                              weights=(1/(err_0[overlap_mask]**2+TINY), 1/(err_rebin**2+TINY))))
+            if scale:
+                sum0 = np.average(flux_0[overlap_mask], weights=1.0/(err_0[overlap_mask]**2+TINY))
+                sum1 = np.average(flux_rebin, weights=1.0/(err_rebin**2+TINY))
+                factor = sum0/sum1
+            else:
+                factor = 1.0
+
+            flux_0[overlap_mask] = np.average((flux_0[overlap_mask], factor*flux_rebin), axis=0, 
+                                               weights=(1/(err_0[overlap_mask]**2+TINY),  1/(factor**2*err_rebin**2+TINY)))
+            err_0[overlap_mask] = np.sqrt(np.average((err_0[overlap_mask]**2, factor**2*err_rebin**2), axis=0,
+                                              weights=(1/(err_0[overlap_mask]**2+TINY), 1/(factor**2*err_rebin**2+TINY))))
 
         else:
             flux_rebin = rebin_spectrum(wave_0[overlap_mask], wave_m, flux_m)
-            flux_0[overlap_mask] = np.average((flux_0[overlap_mask], flux_rebin), axis=0)
-
+            if scale:
+                sum0 = np.average(flux_0[overlap_mask])
+                sum1 = np.average(flux_rebin)
+                factor = sum0/sum1
+            else:
+                factor = 1.0
+            flux_0[overlap_mask] = np.average((flux_0[overlap_mask], factor*flux_rebin), axis=0)
 
         idx = np.where(~overlap_mask_m)[0]
         wave = np.append(wave_0, wave_m[idx])
-        flux = np.append(flux_0, flux_m[idx])
-        err = np.append(err_0, err_m[idx])
+        flux = np.append(flux_0, factor*flux_m[idx])
+        err = np.append(err_0, factor*err_m[idx])
 
         arg_sort = np.argsort(wave)
         wave = wave[arg_sort]
