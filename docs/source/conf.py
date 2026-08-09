2939c6749cdc34f5f919b56b086a113fc12fbdbb
Yan-Rong Li <liyanrong@mail.ihep.ac.cn>
2026-08-09 15:03:05 +0800

    add license.

---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'PyAT'
+copyright = '2026, Yan-Rong Li'
+author = 'Yan-Rong Li'
+
+# The full version, including alpha/beta/rc tags
+release = 'v0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx_rtd_theme', 
+              'numpydoc',
+              'sphinx.ext.mathjax',
+              'sphinx.ext.napoleon',
+              'sphinx.ext.todo',]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# html_theme = 'alabaster'
+# html_static_path = ['_static']
+
+on_rtd = os.environ.get("READTHEDOCS", None) == "True"
+html_theme = "sphinx_book_theme"
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,17 @@
+.. PyAT documentation master file, created by
+   sphinx-quickstart on Sun Jul 26 17:38:46 2026.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to PyAT's documentation
+=================================
+
+A Python package for advanced analysis of astronomical data.
+
+Reference: `Li, Y.-R. & Wang, J.-M., ApJ, submitted <https://ui.adsabs.harvard.edu/abs/2014ApJ...786L...6L/abstract>`_.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   getting_started.rst
